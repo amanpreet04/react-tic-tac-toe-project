810b9b207b29c455e229a19164f166b21516c927
Amanpreet Singh <amanpreetsinghlubana@gmail.com>
2024-07-03 21:06:57 +0530

    Updating game board data immutably

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,17 +1,31 @@
+import { useState } from "react";
+
 const initialBoard = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
 export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialBoard);
+
+  function handleGameBoard(rowIndex, colIndex) {
+    setGameBoard((previousGameBoard) => {
+      const gameBoard = [...previousGameBoard.map((board) => [...board])];
+      gameBoard[rowIndex][colIndex] = "X";
+      return gameBoard;
+    });
+  }
+
   return (
     <ol id="game-board">
-      {initialBoard.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button>{playerSymbol}</button>
+                <button onClick={() => handleGameBoard(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
